554896180c155c6a30832d1bdee65ef208d57533
Fordim <dvformos@gmail.com>
2020-06-06 11:44:09 +0300

    test commit

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
+
 require __DIR__.'/../vendor/autoload.php';
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Psr7\Response as Psr7Response;
+use Slim\Factory\AppFactory;
+
+//phpinfo();
 var_dump('Hello world, test, test');
+
+var_dump(getenv('DATABASE_URL'));
+
+$app = AppFactory::create();
+
+$app->run();
