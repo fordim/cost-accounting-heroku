e9ff7f33a0d31e7d858207fb052f4f30d883578f
Fordim <dvformos@gmail.com>
2020-06-06 11:56:47 +0300

    test commit

--- public/index.php
+++ public/index.php
@@ -8,10 +8,28 @@ use Slim\Psr7\Response as Psr7Response;
 use Slim\Factory\AppFactory;
 
 //phpinfo();
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule->setAsGlobal();
+
+$capsule = new Capsule;
+
 var_dump('Hello world, test, test');
 
-var_dump(getenv('DATABASE_URL'));
+//var_dump(getenv('DATABASE_URL'));
+
+$capsule->addConnection([
+    'url'    => getenv('DATABASE_URL'),
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => ''
+]);
+
+$capsule->setAsGlobal();
 
 $app = AppFactory::create();
+
+var_dump(Capsule::table('users')->get());
 
 $app->run();
