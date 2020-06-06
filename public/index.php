01c336c7e0ecfd98713374aff5376ee337f0a1e1
Fordim <dvformos@gmail.com>
2020-06-06 12:13:47 +0300

    add .env

--- public/index.php
+++ public/index.php
@@ -7,17 +7,14 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Psr7\Response as Psr7Response;
 use Slim\Factory\AppFactory;
 
-//phpinfo();
-
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-$capsule->setAsGlobal();
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
 
 $capsule = new Capsule;
 
 var_dump('Hello world, test, test');
-
-//var_dump(getenv('DATABASE_URL'));
 
 $capsule->addConnection([
     'url'    => getenv('DATABASE_URL'),
